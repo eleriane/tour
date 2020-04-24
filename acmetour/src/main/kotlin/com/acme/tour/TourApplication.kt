d509edef6205ca105ddbc1be8c676cbab3069c36
leonardogloria <A5B7alfazulu>
2020-04-24 16:42:32 -0300

    First Commit

--- acmetour/src/main/kotlin/com/acme/tour/TourApplication.kt
+++ acmetour/src/main/kotlin/com/acme/tour/TourApplication.kt
@@ -3,10 +3,12 @@ package com.acme.tour
 import com.acme.tour.model.Promocao
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Bean
 import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class TourApplication
 
 
